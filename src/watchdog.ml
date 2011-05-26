500198e714e654b4c2f100d0ff29a9fd88c846f8
wleferrand <william@corefarm.com>
2011-05-26 17:59:47 +0200

    ah, of course ..

--- src/watchdog.ml
+++ src/watchdog.ml
@@ -11,13 +11,26 @@ exception ProcessHasExited of int
 
 (* Monitoring function (track the signals) **********************************************)
 
-let rec monitor pid = 
+let rec monitor_ptrace pid = 
   display "Waiting for data from process %d" pid ;
   Lwt_unix.waitpid [ Lwt_unix.WUNTRACED ] pid 
   >>= function 
     | _, (Unix.WSTOPPED s) ->
       display "process %d caught signal %d" pid s ; 
-      Ptrace.cont pid s; monitor pid  
+      Ptrace.cont pid s; monitor_ptrace pid  
+    | _, (Unix.WEXITED s)  ->
+      display "process %d has exited with code %d" pid s ;
+      fail (ProcessHasExited s)
+    | _, (Unix.WSIGNALED s) ->
+      display "process %d was killed by signal %d" pid s ;
+      fail (ProcessHasExited s)
+
+let monitor_std pid = 
+  display "Waiting for data from (std) process %d" pid ; 
+   Lwt_unix.waitpid [] pid 
+  >>= function 
+    | _, (Unix.WSTOPPED s) ->
+      assert false 
     | _, (Unix.WEXITED s)  ->
       display "process %d has exited with code %d" pid s ;
       fail (ProcessHasExited s)
@@ -32,41 +45,23 @@ let attach target pid =
   let process = Lwt_process2.open_process_none_from_pid pid in
   display "Process caught with pid %d" (process#pid) ; 
   (try
-    Ptrace.attach pid ;
-  with _ -> raise (CantAttach pid)); 
-  monitor pid
+     Ptrace.attach pid ;
+   with _ -> raise (CantAttach pid)); 
+  monitor_ptrace pid
 
 let launch (service, process, args) = 
-  let handle = Lwt_process2.open_process_none (process, (Array.of_list args)) in 
+  let handle = Lwt_process.open_process_none (process, (Array.of_list args)) in 
   let pid = handle#pid in
   display "Process %s launched with pid %d" process pid;
-
-  (try
-     display "about to trace"; 
-     Ptrace.attach pid ;
-     display "traced!" ; 
-   with _ -> display "oops" ; raise (CantAttach pid));
-  
-  monitor pid 
-
-(*
-(*  Proc.save_pid service pid 
+  Proc.save_pid service pid 
   >>= fun _ -> 
-*)
-  
-  Lwt_unix.sleep 5.0
-  >>= fun _ -> 
-  
-  display "has killed" ;
-  return ()
-*)
+  monitor_std pid 
+
+
 (* Track a service **********************************************************************)
 
 let track ((service, process, args) as target) =
-  launch target
- 
-(*
-  (* first we look if the process is here *)
+    (* first we look if the process is here *)
   catch 
     (fun () -> 
       Proc.read_pid service
@@ -75,4 +70,4 @@ let track ((service, process, args) as target) =
       | Proc.NoPid _ -> display "There is no pid" ; (* no process, we start a fresh one *) launch target
       | CantAttach _ -> display "Error, can't attach"; launch target 
       | _ as e -> fail e)
-*)
+
